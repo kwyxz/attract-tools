b2bdf319089e96218b523e4f5cfe14bd6733a141
kwyxz <kwyxz@kwyxz.org>
2023-01-16 20:16:05 -0800

    Youtube DL videos webm by default

--- ffmpeg-pi4.sh
+++ ffmpeg-pi4.sh
@@ -17,11 +17,11 @@ if [ $# -lt 1 ]; then
   exit 1
 else
   if [ -z ${4} ]; then
-    ffmpeg -loglevel quiet -stats -filter_threads 4 -i temp-${1}.mp4 -ss ${STARTTIME} -t ${LENGTH} -pix_fmt yuv420p -filter:v scale=640:-1 ${1}.mp4
+    ffmpeg -loglevel quiet -stats -filter_threads 4 -i temp-${1}.webm -ss ${STARTTIME} -t ${LENGTH} -pix_fmt yuv420p -filter:v scale=640:-1 ${1}.mp4
   else
     CROPSIZE=${4}
     echo CROPSIZE=${CROPSIZE}
-    ffmpeg -loglevel quiet -stats -filter_threads 4 -i temp-${1}.mp4 -ss ${STARTTIME} -t ${LENGTH} -pix_fmt yuv420p -filter:v scale=640:-1,crop=${CROPSIZE} ${1}.mp4
+    ffmpeg -loglevel quiet -stats -filter_threads 4 -i temp-${1}.webm -ss ${STARTTIME} -t ${LENGTH} -pix_fmt yuv420p -filter:v scale=640:-1,crop=${CROPSIZE} ${1}.mp4
   fi
   scp ${1}.mp4 pi4:/home/pi/.attract/menu-art/dc/video/
 fi
